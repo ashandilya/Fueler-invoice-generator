40fe3e8c0fa4826467050ba65d428a0262b74f8d
Anshu Shandilya <30520154+ashandilya@users.noreply.github.com>
2025-08-01 00:37:27 +0530

    Fix Vite root configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  root: path.resolve(__dirname),
+  root: '.',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
