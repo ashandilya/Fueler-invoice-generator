81d102812964665f3bc470661932a7e3bcfb5c75
Anshu Shandilya <30520154+ashandilya@users.noreply.github.com>
2025-08-16 01:47:32 +0530

    Fix React import error in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { useState } from "react";
 import { InlineLoginOverlay } from "./components/auth/InlineLoginOverlay";
 import ToastContainer from "./components/common/ToastContainer";
